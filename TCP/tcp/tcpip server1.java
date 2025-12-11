3a42dc65ebb7b8ec593622ffa9fd70c60ec2161e
rakshithac1308 <naikrakshuc08@gmail.com>
2025-12-11 09:35:40 +0530

    Implement TCP server to read and send file contents

---
+++ TCP/tcp/tcpip server1.java
@@ -0,0 +1,31 @@
+tcp program server side 
+import java.net.*;     
+import java.io.*; public class server1   
+{ 
+  public static void main(String args[]) throws Exception 
+  {                           // establishing the connection with the server 
+     ServerSocket sersock = new ServerSocket(4000); 
+     System.out.println("Server ready for connection"); 
+     Socket sock = sersock.accept();            // binding with port: 4000 
+     System.out.println("Connection is successful"); 
+                                                                                                 
+                               // reading the file name from client 
+     InputStream istream = sock.getInputStream( ); 
+     BufferedReader fileRead =new BufferedReader(new InputStreamReader(istream)); 
+     String fname = fileRead.readLine( ); 
+                             // reading file contents 
+     BufferedReader contentRead = new BufferedReader(new FileReader(fname) ); 
+           
+                           // keeping output stream ready to send the contents 
+     OutputStream ostream = sock.getOutputStream( ); 
+     PrintWriter pwrite = new PrintWriter(ostream, true); 
+     
+     String str; 
+     while((str = contentRead.readLine()) !=  null) // reading line-by-line from file 
+     { 
+pwrite.println(str);         // sending each line to client 
+     } 
+  
+     sock.close();  sersock.close();       // closing network sockets      pwrite.close();  fileRead.close(); contentRead.close(); 
+  } 
+}   
